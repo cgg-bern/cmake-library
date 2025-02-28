ccd76462c55a4a7269d6392ee9bf30e4f77bb230
Martin Heistermann <git@mheistermann.de>
2025-02-28 14:46:58 +0100

    MPFR linking fix

--- finders/FindMPFR.cmake
+++ finders/FindMPFR.cmake
@@ -16,9 +16,9 @@ find_package_handle_standard_args(MPFR
     REQUIRED_VARS MPFR_INCLUDE_DIR MPFR_LIBRARY)
 
 if(MPFR_FOUND AND NOT TARGET MPFR::MPFR)
-    add_library(MPFR::MPFR INTERFACE IMPORTED)
+    add_library(MPFR::MPFR UNKNOWN IMPORTED)
     target_include_directories(MPFR::MPFR INTERFACE ${MPFR_INCLUDE_DIR})
-    target_link_libraries(MPFR::MPFR INTERFACE ${MPFR_LIBRARY})
+    set_target_properties(MPFR::MPFR PROPERTIES IMPORTED_LOCATION ${MPFR_LIBRARY})
 endif()
 
 mark_as_advanced(MPFR_INCLUDE_DIR)
